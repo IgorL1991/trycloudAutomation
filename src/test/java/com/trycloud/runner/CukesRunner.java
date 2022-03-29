fe6078fa9fcc03d4641a4e7751618062e4631f6c
Igor Leahu <qbyteexpert@gmail.com>
2022-03-29 14:49:36 -0500

    Jenkins update

--- src/test/java/com/trycloud/runner/CukesRunner.java
+++ src/test/java/com/trycloud/runner/CukesRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/trycloud/step_definitions",
         dryRun = false,
-        tags = "@UploadFileInsideFolder"
+        tags = "@ValidLogIn"
 )
 public class CukesRunner {
 }
